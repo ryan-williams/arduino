b8fae58c43fbeae8f5a01221395d492fb4232d9e
Ryan Williams <ryan.blake.williams@gmail.com>
2013-10-12 01:34:09 -0400

    shorten "Color" helper to "C"

--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -63,7 +63,7 @@ void pulse(byte r, byte g, byte b, int period, int frameLength) {
   int i = 0;
   bool increasing = true;
   while (true) {
-    uint32_t c = Color(r * i / numSteps, g * i / numSteps, b * i / numSteps);
+    uint32_t c = C(r * i / numSteps, g * i / numSteps, b * i / numSteps);
     oneColor(strip, c);
     delay(frameLength);
     if (i >= numSteps) { increasing = false; }
@@ -88,11 +88,11 @@ void setup() {
 
 void loop() {
  // pulse(100, 0, 0, 1000, 50);
-  oneColor(strip, Color(100, 0, 0));
+  oneColor(strip, C(100, 0, 0));
   //rainbow(20);
-//  colorWipe(Color(100, 0, 0), 50);
-//  colorWipe(Color(0, 100, 0), 50);
-//  colorWipe(Color(0, 0, 100), 50);
+//  colorWipe(C(100, 0, 0), 50);
+//  colorWipe(C(0, 100, 0), 50);
+//  colorWipe(C(0, 0, 100), 50);
 //  rainbowCycle(20);
 }
 
@@ -141,7 +141,7 @@ void colorWipe(uint32_t c, uint8_t wait) {
 /* Helper functions */
 
 // Create a 24 bit color value from R,G,B
-uint32_t Color(byte r, byte g, byte b)
+uint32_t C(byte r, byte g, byte b)
 {
   uint32_t c;
   c = r;
@@ -157,12 +157,12 @@ uint32_t Color(byte r, byte g, byte b)
 uint32_t Wheel(byte WheelPos)
 {
   if (WheelPos < 85) {
-   return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+   return C(WheelPos * 3, 255 - WheelPos * 3, 0);
   } else if (WheelPos < 170) {
    WheelPos -= 85;
-   return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+   return C(255 - WheelPos * 3, 0, WheelPos * 3);
   } else {
    WheelPos -= 170; 
-   return Color(0, WheelPos * 3, 255 - WheelPos * 3);
+   return C(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
