9b3ca31930185605501f6dffc14f633cd74c7f00
Ryan Williams <ryan.blake.williams@gmail.com>
2014-02-23 01:39:35 -0500

    add RGB color-tracking bars

--- www/client/colors.js
+++ www/client/colors.js
@@ -53,25 +53,101 @@ $(function() {
 
   Math.seedrandom(3);
 
-  svg = d3.select('svg');
-  for (i = 0; i < numBoxes; ++i) {
-    svg.append('circle');
-  }
-
-  var circleCoords = planarRandomWalk(100, 100, 2*R + 5, numBoxes);
+  var circleCoords = planarRandomWalk(350, 100, 2*R + 5, numBoxes);
 
   var initialColor = { r: maxAbs / 2, g: maxAbs / 2, b: maxAbs / 2 };
-  var colors = genArray(initialColor, function(prev) { return { r:0, g:0, b:0 }; }, numBoxes);
+  var colors = genArray(
+      initialColor,
+      function(prev) { return initialColor; },
+      numBoxes
+  );
 
-  svg.selectAll('circle')
-      .attr("r", R)
+  svg = d3.select('#svg');
+  svg.selectAll('circle.pixel')
       .data(circleCoords)
+      .enter()
+      .append('circle')
+      .attr('class', 'pixel')
+      .attr("r", R)
       .attr('cx', function(d,i) {
         return Math.floor(d.x);
       })
       .attr('cy', function(d,i) {
         return Math.floor(d.y);
       });
+
+  var numLineWidth = 300;
+  var serifHeight = 10;
+
+  function numLine(startPoint, color, updateFn) {
+    return {
+      updateFn: updateFn,
+      lines: [
+        {
+          start: { x: startPoint.x, y: startPoint.y - serifHeight },
+          end: { x: startPoint.x, y: startPoint.y + serifHeight },
+          color: color
+        },
+        {
+          start: { x: startPoint.x, y: startPoint.y },
+          end: { x: startPoint.x + numLineWidth, y: startPoint.y },
+          color: color
+        },
+        {
+          start: { x: startPoint.x + numLineWidth, y: startPoint.y - serifHeight },
+          end: { x: startPoint.x + numLineWidth, y: startPoint.y + serifHeight },
+          color: color
+        }
+      ]
+    }
+  }
+
+  var numLines = [
+    numLine({x:20,y: 50}, '#F00', function() { return r; }),
+    numLine({x:20,y:100}, '#0F0', function() { return g; }),
+    numLine({x:20,y:150}, '#00F', function() { return b; })
+  ];
+
+  svg.selectAll('g.numlines')
+      .data(numLines)
+      .enter()
+      .append('g')
+      .attr('class', 'numlines')
+      .selectAll('line')
+      .data(function(d) { return d.lines; })
+      .enter()
+      .append('line')
+      .attr('x1',function(d){return d.start.x;})
+      .attr('y1',function(d){return d.start.y;})
+      .attr('x2',function(d){return d.end.x;})
+      .attr('y2',function(d){return d.end.y;})
+      .style('stroke-width', 2)
+      .style('stroke', function(d) { return d.color; })
+  ;
+
+  function rgb(d,i) {
+    console.log(d);
+    console.log(i);
+    console.log([[r,g,b][i]]);
+    return [[r,g,b][i]];
+  }
+
+  svg.selectAll('g.numlines')
+      .selectAll('circle')
+      .data(function(d) {
+        return [{
+          fn: d.updateFn,
+          cy: d.lines[1].start.y,
+          minX: d.lines[1].start.x,
+          maxX: d.lines[1].end.x
+        }];
+      })
+      .enter()
+      .append('circle')
+      .attr('class', 'pointer')
+      .attr('r', 5)
+      .attr('fill', '#000')
+  ;
 
   var r = maxAbs / 2;
   var g = maxAbs / 2;
@@ -104,11 +180,21 @@ $(function() {
     if (b >= maxAbs || b <= minAbs) bv = 0;
 
     shift(colors);
-    colors[0] = {r:r,g:g,b:b};
-    svg.selectAll('circle')
+    colors[0] = { r:r, g:g, b:b };
+    svg.selectAll('circle.pixel')
         .attr('fill', function(d, i) {
           return rgbString(colors[i].r, colors[i].g, colors[i].b);
         });
+
+    svg.selectAll('g.numlines')
+        .selectAll('circle')
+      .attr('cx', function(d) {
+        return d.minX + (d.maxX - d.minX) * (Math.floor(d.fn()) - minAbs) / (maxAbs - minAbs);
+      })
+      .attr('cy', function(d) {
+        return Math.floor(d.cy);
+      })
+    ;
 
   }
 
